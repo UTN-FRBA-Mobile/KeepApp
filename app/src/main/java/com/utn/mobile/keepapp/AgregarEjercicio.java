c6ed00a1e2588af8bb111e6a5beb76270919374c
Julian Salischiker <jusalis@gmail.com>
2017-09-24 01:25:44 -0300

    Cambios visuales de pantalla de agregar ejercicio.

--- app/src/main/java/com/utn/mobile/keepapp/AgregarEjercicio.java
+++ app/src/main/java/com/utn/mobile/keepapp/AgregarEjercicio.java
@@ -1,7 +1,7 @@
 package com.utn.mobile.keepapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
