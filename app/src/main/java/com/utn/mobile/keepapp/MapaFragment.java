29584bff2bf89a1c0c30001604b0892d4f090c70
norchow <nicolas.orchow@valtech.com>
2017-09-24 16:01:15 -0300

    Permite agregar gimnasios en el mapa
    Se guardan en Firebase los nombres y las posiciones de los gimnasios
    para ser mostrados posteriormente.

--- app/src/main/java/com/utn/mobile/keepapp/MapaFragment.java
+++ app/src/main/java/com/utn/mobile/keepapp/MapaFragment.java
@@ -9,24 +9,40 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.utn.mobile.keepapp.domain.Gimnasio;
 
-
-public class MapaFragment extends Fragment implements OnMapReadyCallback {
+public class MapaFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
     private GoogleMap mapa;
+    private Marker currentMarker;
     private Activity activity;
     private Location myLocation;
+    private LinearLayout form;
+    private DatabaseReference mDatabase;
+    private EditText inputName;
 
     public MapaFragment() {
         // Required empty public constructor
@@ -43,15 +59,55 @@ public class MapaFragment extends Fragment implements OnMapReadyCallback {
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        return v;
+        FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference("usuarios/".concat(currentFirebaseUser.getUid()).concat("/gimnasios"));
 
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        form = (LinearLayout) getView().findViewById(R.id.markerForm);
+        inputName = (EditText) getView().findViewById(R.id.inputName);
+
+        final ImageButton saveButton = (ImageButton) getView().findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                hideForm();
+                String nombre = inputName.getText().toString();
+                Gimnasio nuevoGimnasio = new Gimnasio(nombre, currentMarker.getPosition().latitude, currentMarker.getPosition().longitude);
+                currentMarker.setTitle(nombre);
+
+                mDatabase.push().setValue(nuevoGimnasio);
+                Toast.makeText(getContext(), "El gimnasio ha sido guardado satisfactoriamente", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final ImageButton deleteButton = (ImageButton) getView().findViewById(R.id.deleteButton);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                hideForm();
+            }
+        });
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
         mapa = map;
         getMyLocation();
+        mapa.setOnMapLongClickListener(this);
     }
+
+    @Override
+    public void onMapLongClick(LatLng point) {
+        currentMarker = mapa.addMarker(new MarkerOptions()
+            .position(point)
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+
+        form.setVisibility(View.VISIBLE);
+    }
+
 
     private void getMyLocation() {
         if (ActivityCompat.checkSelfPermission(activity, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -96,4 +152,9 @@ public class MapaFragment extends Fragment implements OnMapReadyCallback {
             getMyLocation();
         }
     }
+
+    private void hideForm() {
+        form.setVisibility(View.GONE);
+    }
+
 }
---
+++ app/src/main/java/com/utn/mobile/keepapp/domain/Gimnasio.java
@@ -0,0 +1,41 @@
+package com.utn.mobile.keepapp.domain;
+
+/**
+ * Created by norchow on 9/24/17.
+ */
+
+public class Gimnasio {
+    public String nombre;
+    public double latitud;
+    public double longitud;
+
+    public Gimnasio(String nombre, double latitud, double longitud) {
+        this.nombre = nombre;
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+}
